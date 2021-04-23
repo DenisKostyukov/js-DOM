68f4bb15710ffd288299b6946f9dc9d0683f4486
DenisKostyukov <kostukov08@gmail.com>
2021-04-23 11:59:52 +0300

    add parent styles

--- assets/js/sandox.js
+++ assets/js/sandox.js
@@ -1,6 +1,19 @@
-const btns = document.querySelectorAll('.link-button');
+const btns = document.querySelectorAll('button');
+/*btn.addEventListener('click', ({
+  target
+}) => {
+  target.style.backgroundColor = 'red';
+})*/
+const getColor = ({
+  target: {
+    parentNode,
+    dataset: {
+      color
+    }
+  }
+}) => {
+    parentNode.style = `background-color: ${color}`;
+};
 for (const btn of btns) {
-  btn.addEventListener('click', ({target:{dataset:{link}}}) => {
-    console.log(link)
-  })
+  btn.addEventListener('click', getColor)
 }

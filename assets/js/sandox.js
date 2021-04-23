41b69a6012fce1a833b72bf2d0c5795f6a8dde59
DenisKostyukov <kostukov08@gmail.com>
2021-04-23 11:02:17 +0300

    data-* example

--- assets/js/sandox.js
+++ assets/js/sandox.js
@@ -1,13 +1,6 @@
-const [btn1, btn2] = document.querySelectorAll('.button');
-/*const btnEnterHandler = () =>{
-  const temp = btn1.innerText;
-  btn1.innerText=btn2.innerText;
-  btn2.innerText=temp;
+const btns = document.querySelectorAll('.link-button');
+for (const btn of btns) {
+  btn.addEventListener('click', ({target:{dataset:{link}}}) => {
+    console.log(link)
+  })
 }
-  btn1.addEventListener('mouseenter', btnEnterHandler)
-  btn2.addEventListener('mouseenter', btnEnterHandler)*/
-  const btnEnterHandler = () =>{
-    [btn1.innerText, btn2.innerText] = [btn2.innerText, btn1.innerText]
-  }
-  btn1.addEventListener('mouseenter', btnEnterHandler)
-  btn2.addEventListener('mouseenter', btnEnterHandler)

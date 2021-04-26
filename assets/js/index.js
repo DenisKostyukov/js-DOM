38a2c60e1f0b87b6ae43ed275d4d68e9b606e996
DenisKostyukov <kostukov08@gmail.com>
2021-04-26 12:09:52 +0300

    add cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,13 +9,13 @@ const imageDB = [
 ]
 
 const slider = new Slider(imageDB);
-const [nextButton, prevButton]= document.querySelectorAll('.slider-container > button');
+const [nextButton, prevButton] = document.querySelectorAll('.slider-container > button');
 const sliderImage = document.querySelector('.slide');
 
 updateView(slider.currentSlide);
 
-function createButton(action = "next"){
-  return () =>{
+function createButton(action = "next") {
+  return () => {
     const newImage = slider[action]();
     updateView(newImage);
   }
@@ -23,6 +23,75 @@ function createButton(action = "next"){
 prevButton.addEventListener("click", createButton("prev"));
 nextButton.addEventListener("click", createButton("next"));
 
-function updateView(imgLink){
+function updateView(imgLink) {
   sliderImage.setAttribute('src', imgLink);
+}
+/*Cards*/
+
+const cardContainer = document.getElementById('root');
+
+const cardElement = data.map((place) => createPlaceCards(place));
+cardContainer.append(...cardElement)
+function createPlaceCards(place) {
+  const card = document.createElement('li');
+  card.classList.add('cardWrapper');
+
+  const container = document.createElement('article');
+  container.classList.add('cardContainer');
+
+  const imageWrapper = document.createElement('div');
+  imageWrapper.classList.add('cardImageWrapper');
+  imageWrapper.style.backgroundColor = stringToColour(place.name || "");
+
+  const initials = document.createElement('div');
+  initials.classList.add('initials');
+  initials.append(document.createTextNode(place.name[0] || ""));
+
+  const image = document.createElement('img');
+  image.classList.add('cardImage');
+  image.src = place.profilePicture;
+  imageWrapper.append(initials,image);
+  image.hidden = true;
+  image.addEventListener('error',handlerImageError )
+  image.addEventListener('load',handlerImageLoad )
+  
+  const contentWrapper = document.createElement('div');
+  contentWrapper.classList.add('contentWrapper');
+
+  const name = document.createElement('h3');
+  name.classList.add('cardName');
+  name.append(document.createTextNode(place.name || ""));
+
+  const description = document.createElement("p");
+  description.classList.add('cardDescription');
+  description.append(document.createTextNode(place.description || ""));
+
+  contentWrapper.append(name, description);
+
+  container.append(imageWrapper, contentWrapper);
+  card.append(container);
+
+  return card;
+}
+
+  function stringToColour(str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  var colour = '#';
+  for (var i = 0; i < 3; i++) {
+    var value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
+}
+
+/*event listeners*/
+
+function handlerImageError({target}){
+  target.remove();
+}
+function handlerImageLoad({target}){
+  target.hidden=false;
 }
